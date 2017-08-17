d6f32c93ed2c8e8f6a8c9d015073cfb579490dae
shanebouverie <shane@wearehi5.com>
2017-08-16 19:55:12 -0500

    add CYOA folder with index.html and cyoa.js

--- media/greeting.js
+++ media/greeting.js
@@ -1,3 +1,5 @@
-var name = prompt("Hi Mate! What's your name?");
+var firstName = prompt("Hi there! What's your first name?") || "Visitor";
+var lastName = prompt("What's your last name?") || "McDefaultson";
 var output = document.querySelector('.greeting');
-output.innerHTML = "<p>Thanks for visiting, " +name+ ".</p>";
+
+output.innerHTML = "<p>Thanks for visiting, " + firstName + " " + lastName + ".</p>";
