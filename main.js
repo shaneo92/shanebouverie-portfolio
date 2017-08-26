141a05804a29fd930ca65e276f1428f5bb0aaedb
shanebouverie <shane@wearehi5.com>
2017-08-26 10:48:41 -0500

    Worked with objects on contact info

--- main.js
+++ main.js
@@ -3,6 +3,64 @@ var contactArray = [
   '<li><a href="https://www.linkedin.com/in/shane-bouverie-3256a747">Linkedin</a></li>'
 ]
 
+
+var contactObject = {
+    "name": {
+        "firstName": "Professor",
+        "lastName": "Chaos"
+    },
+    "email": "professor@professorchaos.com",
+    "twitter": "@ProfChaos",
+    "quote": "Prepare to meet thy doom"
+}
+
+contactObject['phone'] = '123456789'
+contactObject['facebook'] = 'my facebook'
+
+var quote = 'Carpe Diem'
+
+contactObject['quote'] = quote
+
+contactObject.quote = [
+    "Prepare to meet thy doom",
+    "Nobody expects Professor Chaos",
+    "Bow before the power of the DARK SIDE"
+]
+
+// contactObject.getGithub = function() {
+//   return contactObject.github
+// }
+
+console.log(contactObject)
+
+var elementLister = function(contactElement) {
+  var listString = "<li> " + contactElement + "</li>";
+
+  if (Array.isArray(contactElement)) {
+    listString ="<ol>"
+
+    contactElement.forEach (function(element) {
+      listString += "<li>" + element + "</li>";
+    })
+
+    listString += "</ol>"
+  }else if(typeof contactElement === "object"){
+    listString = "<li>";
+
+    for(var element in contactElement){
+      listString += contactElement[element] + " ";
+    }
+
+    listString += "</li>";
+  }
+
+  $(".second-list").append(listString);
+};
+
+// For in loop
+// for(var contactType in contactObject){
+//   elementLister( contactObject[contactType] );
+// }
 //While Loop
 // var i = 0
 //
@@ -25,7 +83,7 @@ var contactArray = [
 //
 // appendStrings(contactArray, 'li')
 
-//forEach
+// forEach
 contactArray.forEach(function(item){
   $('.second-list').append(item)
 })
