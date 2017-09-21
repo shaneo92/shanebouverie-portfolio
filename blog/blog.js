ebe1696257e32f50d7dc1c989237c8b9f75318c6
shanebouverie <shane@wearehi5.com>
2017-09-20 22:57:00 -0500

    Add some half decent styling, add sidebar, add table, add divs on homepage and add media queries

--- blog/blog.js
+++ blog/blog.js
@@ -1,13 +1,13 @@
-var getPosts = $.ajax('https://jsonplaceholder.typicode.com/posts')
-
-var handlePost = function(post){
-  var title = "<h3>" + post.title + "</h3>";
-  var content = "<p>" + post.body + "</p>";
-  var blurb = "<div class='post'>" + title + content + "</div>";
-
-  $( ".text-section" ).append( blurb );
-}
-
-getPosts.then(
-    ( posts ) => posts.forEach( handlePost )
-);
+// var getPosts = $.ajax('https://jsonplaceholder.typicode.com/posts')
+//
+// var handlePost = function(post){
+//   var title = "<h3>" + post.title + "</h3>";
+//   var content = "<p>" + post.body + "</p>";
+//   var blurb = "<div class='post'>" + title + content + "</div>";
+//
+//   $( ".text-section" ).append( blurb );
+// }
+//
+// getPosts.then(
+//     ( posts ) => posts.forEach( handlePost )
+// );
